ebf5453f5e34a681c261a1b979d4c2b5053e09df
Shahnoor <shahnoor.arshid@theowlsense.com>
2023-04-12 10:54:12 +0500

    test

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ num2 = 30.0
 sum = num1 + num2
 
 # Display the sum
-console.log('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
    
 
 
